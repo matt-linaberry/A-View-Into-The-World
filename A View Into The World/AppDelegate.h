c23e9e980b35144e70754fc05f5f3efef7f432e4
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-04 21:03:29 -0500

    Initial Commit

---
+++ A View Into The World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A View Into The World
+//
+//  Created by Matthew Linaberry on 12/4/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A View Into The World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A View Into The World
+//
+//  Created by Matthew Linaberry on 12/4/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
