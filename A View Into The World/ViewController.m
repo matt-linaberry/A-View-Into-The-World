5c523869c9f457d4b2aeea105b9014a732f10eab
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-10 17:04:56 -0500

    Challenge 13
    This includes right before challenge 13

--- A View Into The World/ViewController.m
+++ A View Into The World/ViewController.m
@@ -17,11 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"button 1 bounds size(%f, %f)", self.button1.bounds.size.height, self.button1.bounds.size.width);
-    NSLog(@"button 1 frame size(%f, %f)", self.button1.frame.size.height, self.button1.frame.size.width);
-    NSLog(@"button 1 bounds origin(%f, %f)", self.button1.bounds.origin.x, self.button1.bounds.origin.y);
-    NSLog(@"button 1 bounds frame origin(%f, %f)", self.button1.frame.origin.x, self.button1.frame.origin.y);
-    NSLog(@"button 1 bounds center(%f, %f)", self.button1.center.x, self.button1.center.y);
+    CGRect myViewsFrame = CGRectMake(20, 250, 200, 60);
+    UIView *myView = [[UIView alloc] initWithFrame:myViewsFrame];
+    myView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:myView];
+    
+    UIButton *anotherButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    anotherButton.frame = CGRectMake(40, 40, 200, 100);
+    anotherButton.backgroundColor = [UIColor greenColor];
+    [anotherButton setTitle:@"Press me please!" forState:UIControlStateNormal];
+    [self.view addSubview:anotherButton];
+    [myView removeFromSuperview];
+    
+    // add a target action
+    [anotherButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+- (void) didPressButton:(UIButton *)button
+{
+    NSLog(@"Hey! You!  this is from code!");
 }
 
 - (void)didReceiveMemoryWarning {
